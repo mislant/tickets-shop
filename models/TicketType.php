88f4b31700b18855e54a7084e570bd82c1d72d6f
Kiryha Shutnik <Kiryakz2000@gmail.com>
2019-12-21 16:29:48 +0600

    Made RBAC and admin panel.Migartions changed

--- models/TicketType.php
+++ models/TicketType.php
@@ -4,16 +4,22 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
-class Ticket_type extends ActiveRecord
+class TicketType extends ActiveRecord
 {
+    public static function tableName()
+    {
+        return '{{%ticket_type}}';
+    }
+
     public function attributeLabels()
     {
         return
-        [
-            'id' => 'ID',
-            'type' => 'Type'
-        ];
+            [
+                'id' => 'ID',
+                'type' => 'Type'
+            ];
     }
+
     public static function GiveAll()
     {
         $ticket_type = new Ticket_type;
--- views/event/events_ticket_create.php
+++ views/event/events_ticket_create.php
@@ -16,7 +16,7 @@ $form = ActiveForm::begin([
 $ticket_types = Ticket_type::find()->all();
 $type = ArrayHelper::map($ticket_types,'id','type');
 $params = [
-    'promit' => 'Укажите тип билета'
+    'prompt' => 'Укажите тип билета'
 ]
 ?>
 <div class="form-group row">
@@ -24,7 +24,10 @@ $params = [
     <?=$form->field($model,'cost')->textInput(['placeholder' => 'Укажите цену билета']) ?>
     <?=$form->field($model,'amount')->textInput(); ?>
 </div>
+<div class="form-row">
 <?= Html::submitButton('Add More',['class' => 'btn btn-success']) ?>
-<?= Html:: a('End',['/event/total-tickets'],['class' => 'btn btn-primary'])?>
+<?= Html:: a('End',['/event/total-tickets','id' => $model->event_id ],['class' => 'btn btn-primary'])?>
+</div>
+
 
 <?php ActiveForm::end() ?>
